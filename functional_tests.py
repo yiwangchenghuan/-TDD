994a29e02e62fb30b589100f226772dd2c6490e4
unknown <2238339460@qq.com>
2020-04-29 21:13:07 +0800

    First refactoring completed!

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 乔伊听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
@@ -19,7 +24,6 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-
 
         # 应用邀请她输入一个待办事项
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -33,10 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项显示'1：Buy peacock feathers'
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面又显示了一个文本框，可以输入其他待办事项
         # 她输入了'Use peacock feathers to make a fly'(做假蝇)
@@ -49,8 +50,8 @@ class NewVisitorTest(unittest.TestCase):
         # 页面再次刷新，她的清单显示两条事项
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly ', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # 她想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
