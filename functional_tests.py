feadb8fba6b23e3cebcf92bb8c765c09ff43399f
unknown <2238339460@qq.com>
2020-04-29 17:12:39 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,34 +1,47 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
 
-class NewVisitorTest(unittest.TestCase): #(1)
-    def setUp(self):       #(3) 测试执行前
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDown(self):    #(3) 测试方法执行后
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):    #(2) 测试方法
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # 乔伊听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
         self.browser.get('http://localhost:8000')
 
         # 她注意到网页的标题和头部包含'To-Do'这个词
-        self.assertIn('To-Do',self.browser.title)           #(4) 测试失败执行
-        self.fail('Finish the test!')  #(5)
+        self.assertIn('To-Do',self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
 
         # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # 她在文本中输入了'Buy peacock feathers'（购买孔雀羽毛）
         # 她的爱好是钓鱼
+        inputbox.send_keys('Buy peacock feathers')
 
         # 她输入回车键后页面刷新了
         # 待办事项显示'1：Buy peacock feathers'
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
 
         # 页面又显示了一个文本框，可以输入其他待办事项
         # 她输入了'Use peacock feathers to make a fly'(做假蝇)
         # 她做事非常有条理
-
+        self.fail('Finish the test!')
         # 页面再次刷新，她的清单显示两条事项
 
         # 她想知道这个网站是否会记住她的清单
@@ -39,5 +52,5 @@ class NewVisitorTest(unittest.TestCase): #(1)
 
         # 她很满意，去休息了
 
-if __name__ == '__main__': #(6)
-    unittest.main(warnings='ignore')    #(7)禁止抛出ResourceWarning
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
